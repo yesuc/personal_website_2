d4f01337122a34d060436f5d932e35a998d3b806
yesuc <ycarter@colgate.edu>
2019-11-15 15:58:27 -0500

    added bootstrap support. fixed navbar

--- spec/home_controller_spec.rb
+++ spec/home_controller_spec.rb
@@ -3,14 +3,14 @@ require 'rails-controller-testing'
 
 RSpec.describe HomeController, type: :controller do
   render_views
-  
+
   describe "GET #index" do
     it "should GET #index" do
       get :index
       expect(response).to have_http_status(:success)
     end
 
-    it "should render a the application layout" do
+    it "should render the application layout" do
       get :index
       expect(response).to render_template("layouts/application")
     end
